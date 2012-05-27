3f69d8a763ddfb927017986a23f57901342b83d8
Ross Hunter <ross-hunter@ross-hunter.com>
2012-05-27 18:19:27 -0400

    responsive

--- sites/default/themes/grwebdev/templates/page.tpl.php
+++ sites/default/themes/grwebdev/templates/page.tpl.php
@@ -72,7 +72,7 @@
 
 <div id="page">
 
-  <header id="header" role="banner">
+  <header id="header" role="banner" class="clearfix">
 
     <?php if ($logo): ?>
       <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
@@ -126,7 +126,7 @@
           print theme('links__system_main_menu', array(
             'links' => $main_menu,
             'attributes' => array(
-              'class' => array('links', 'inline', 'clearfix'),
+              'class' => array('links', 'clearfix'),
             ),
             'heading' => array(
               'text' => t('Main menu'),
@@ -137,28 +137,28 @@
         </nav>
       <?php endif; ?>
 
-      <div id="content" class="column" role="main">
-        <?php print render($page['highlighted']); ?>
-        <?php print $breadcrumb; ?>
-        <a id="main-content"></a>
-        <?php print render($title_prefix); ?>
-        <?php if ($title): ?>
-          <h1 class="title" id="page-title"><?php print $title; ?></h1>
-        <?php endif; ?>
-        <?php print render($title_suffix); ?>
-        <?php print $messages; ?>
-        <?php print render($tabs); ?>
-        <?php print render($page['help']); ?>
-        <?php if ($action_links): ?>
-          <ul class="action-links"><?php print render($action_links); ?></ul>
-        <?php endif; ?>
-        <?php print render($page['content']); ?>
-        <?php print $feed_icons; ?>
-      </div><!-- /#content -->
-
       <?php print render($page['navigation']); ?>
 
     </div><!-- /#navigation -->
+
+    <div id="content" class="column" role="main">
+      <?php print render($page['highlighted']); ?>
+      <?php print $breadcrumb; ?>
+      <a id="main-content"></a>
+      <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <h1 class="title" id="page-title"><?php print $title; ?></h1>
+      <?php endif; ?>
+      <?php print render($title_suffix); ?>
+      <?php print $messages; ?>
+      <?php print render($tabs); ?>
+      <?php print render($page['help']); ?>
+      <?php if ($action_links): ?>
+        <ul class="action-links"><?php print render($action_links); ?></ul>
+      <?php endif; ?>
+      <?php print render($page['content']); ?>
+      <?php print $feed_icons; ?>
+    </div><!-- /#content -->
 
     <?php
       // Render the sidebars to see if there's anything in them.
